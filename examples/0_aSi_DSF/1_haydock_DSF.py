dcc5745ce94945865190a16c47c3d7f2c5576a13
Alfredo Fiorentino <alfredofiorentino@hotmail.it>
2025-12-04 01:06:51 +0100

    update multiprocessing for Mac/Windows (add __init__==main). Add plot linewidths in example

--- examples/0_aSi_DSF/1_haydock_DSF.py
+++ examples/0_aSi_DSF/1_haydock_DSF.py
@@ -8,39 +8,39 @@ import time
 sys.setrecursionlimit(2000)
 
 ### Initialize Vibrational System and Spectra
+if __name__ == '__main__':
+    root='N1728'
+    system=vibrationalsystem.VibrationalSystem(root=root)
+    start=time.time()
+    spectra=vibrationalspectra.VibrationalSpectra(vibrational_system=system,options={'hl_steps':200,'eta':1.0})
+    spectrum=spectra.compute_vdsf(nq=8)
+    end=time.time()
+    print('diagonalization time:{} s'.format(end-start) )
 
-root='N1728'
-system=vibrationalsystem.VibrationalSystem(root=root)
-start=time.time()
-spectra=vibrationalspectra.VibrationalSpectra(vibrational_system=system,options={'hl_steps':200,'eta':1.0})
-spectrum=spectra.compute_vdsf(nq=8)
-end=time.time()
-print('diagonalization time:{} s'.format(end-start) )
+    ### Plotting
+    n_modes=2  # Longitudinal and Transverse
+    q=spectrum['Q']
+    q=np.linalg.norm(q,axis=1)
+    w=spectrum['omega']
+    S_qw=np.zeros((n_modes,len(q),len(w)))
+    for ib,b in enumerate(['L','T']):
+        for iq in range(len(q)):
+            S_qw[ib,iq,:]=spectrum[iq][b]['S']
 
-### Plotting
-n_modes=2  # Longitudinal and Transverse
-q=spectrum['Q']
-q=np.linalg.norm(q,axis=1)
-w=spectrum['omega']
-S_qw=np.zeros((n_modes,len(q),len(w)))
-for ib,b in enumerate(['L','T']):
-    for iq in range(len(q)):
-        S_qw[ib,iq,:]=spectrum[iq][b]['S']
-
-fig,axes=plt.subplots(ncols=2,sharey=True)
-vmax=1e-2
-for imode in range(n_modes):
-    ax=axes[imode]
-    p=ax.pcolormesh(q,w/2/np.pi,w[:,np.newaxis]/2/np.pi*S_qw[imode,:,:].T,
-                          shading = 'nearest',
-                          vmin = 0,
-                          vmax = vmax,
-                          cmap = 'inferno'
-                 )
-    dispersion=w[np.argmax(w[np.newaxis,:]*S_qw[imode,:,:],axis=1)] ### it should give the acoustic dispersion in the lower frequency part
-    # print(T_dispersion.shape)
-    ax.plot(q,dispersion/2/np.pi,'--',color='b')
-    if imode==1:
-        fig.colorbar(p, ax=ax)
-fig.suptitle('$S(q,\\nu)$')
-plt.show()
+    fig,axes=plt.subplots(ncols=2,sharey=True)
+    vmax=1e-2
+    for imode in range(n_modes):
+        ax=axes[imode]
+        p=ax.pcolormesh(q,w/2/np.pi,w[:,np.newaxis]/2/np.pi*S_qw[imode,:,:].T,
+                              shading = 'nearest',
+                              vmin = 0,
+                              vmax = vmax,
+                              cmap = 'inferno'
+                     )
+        dispersion=w[np.argmax(w[np.newaxis,:]*S_qw[imode,:,:],axis=1)] ### it should give the acoustic dispersion in the lower frequency part
+        # print(T_dispersion.shape)
+        ax.plot(q,dispersion/2/np.pi,'--',color='b')
+        if imode==1:
+            fig.colorbar(p, ax=ax)
+    fig.suptitle('$S(q,\\nu)$')
+    plt.show()
--- examples/0_aSi_DSF/2_VDOS.py
+++ examples/0_aSi_DSF/2_VDOS.py
@@ -10,62 +10,62 @@ import matplotlib.pyplot as plt
 import time
 
 ### Initialize Vibrational System and Spectra
+if __name__ == '__main__':
+    root='N1728'
+    system=vibrationalsystem.VibrationalSystem(root=root)
+    spectra=vibrationalspectra.VibrationalSpectra(vibrational_system=system,options={'hl_steps':300,'eta':1.0})
 
-root='N1728'
-system=vibrationalsystem.VibrationalSystem(root=root)
-spectra=vibrationalspectra.VibrationalSpectra(vibrational_system=system,options={'hl_steps':300,'eta':1.0})
+    ### Compute Stochastic VDOS# of nstoc random vectors
+    start=time.time()
+    VDOS=spectra.compute_stochastic_vdos(nstoc=10)
+    end=time.time()
+    print('Haydock wall time:{} s'.format(end-start) )
 
-### Compute Stochastic VDOS# of nstoc random vectors
-start=time.time()
-VDOS=spectra.compute_stochastic_vdos(nstoc=20)
-end=time.time()
-print('Haydock wall time:{} s'.format(end-start) )
-
-### Plotting
-prefactor_error=3
-try:
-    nstoc
-except NameError:
-    nstoc = len(VDOS)
-print('nstoc=',nstoc)
-fig,ax=plt.subplots()
-w=spectra.options.omega_array
-y=np.mean(np.array([w*VDOS[i]['S'] for i in range(nstoc)]),axis=0)
-yerr=prefactor_error*np.std(np.array([w*VDOS[i]['S'] for i in range(nstoc)]),axis=0)/np.sqrt(nstoc)
-norm=np.trapz(y, w)
-y/=norm
-yerr/=norm
-ax.plot(w,y,label='stochastic n={}'.format(nstoc))
-ax.fill_between(w,y+yerr,y-yerr,alpha=0.5,label=f'{prefactor_error} $\sigma$')
-plt.ylabel('VDOS')
-plt.xlabel('$\omega\mathrm{~(rad/ps)}$')
-plt.legend()
-plt.show()
+    ### Plotting
+    prefactor_error=3
+    try:
+        nstoc
+    except NameError:
+        nstoc = len(VDOS)
+    print('nstoc=',nstoc)
+    fig,ax=plt.subplots()
+    w=spectra.options.omega_array
+    y=np.mean(np.array([w*VDOS[i]['S'] for i in range(nstoc)]),axis=0)
+    yerr=prefactor_error*np.std(np.array([w*VDOS[i]['S'] for i in range(nstoc)]),axis=0)/np.sqrt(nstoc)
+    norm=np.trapz(y, w)
+    y/=norm
+    yerr/=norm
+    ax.plot(w,y,label='stochastic n={}'.format(nstoc))
+    ax.fill_between(w,y+yerr,y-yerr,alpha=0.5,label=f'{prefactor_error} $\sigma$')
+    plt.ylabel('VDOS')
+    plt.xlabel('$\omega\mathrm{~(rad/ps)}$')
+    plt.legend()
+    plt.show()
 
 
 
-#### Compute direct VDOS for comparison
-dynmat_dense=spectra.vs.dynmat.toarray()
-start=time.time()
-w2,eig=np.linalg.eigh(dynmat_dense)
-end=time.time()
-print('diagonalization time:{} s'.format(end-start) )
-eta=spectra.options.eta
-w_true=np.sign(w2)*np.sqrt(np.abs(w2))
+    #### Compute direct VDOS for comparison
+    dynmat_dense=spectra.vs.dynmat.toarray()
+    start=time.time()
+    w2,eig=np.linalg.eigh(dynmat_dense)
+    end=time.time()
+    print('diagonalization time:{} s'.format(end-start) )
+    eta=spectra.options.eta
+    w_true=np.sign(w2)*np.sqrt(np.abs(w2))
 
-fig,ax=plt.subplots()
+    fig,ax=plt.subplots()
 
-ax.plot(w,y,label='stochastic n={}'.format(nstoc))
-ax.fill_between(w,y+yerr,y-yerr,alpha=0.5,label=f'{prefactor_error} $\sigma$')
+    ax.plot(w,y,label='stochastic n={}'.format(nstoc))
+    ax.fill_between(w,y+yerr,y-yerr,alpha=0.5,label=f'{prefactor_error} $\sigma$')
 
-# vdos=compute_vdos(w_true,w,sigma=eta/2,kernel='lorentz')
-# ax.plot(w,vdos,'--',label='direct Lor.')
-vdos=compute_vdos(w_true,w,sigma=eta,kernel='DHO')
-ax.plot(w,vdos,'-.',label='direct') #DHO
-chemical_f=spectra.vs.atoms.get_chemical_symbols()[0]
-N=spectra.vs.atoms.get_global_number_of_atoms()
-plt.title(f'a{chemical_f} $N={N}$ $\eta={eta}$' )
-plt.legend()
-plt.ylabel('VDOS')
-plt.xlabel('$\omega\mathrm{~(rad/ps)}$')
-plt.show()
+    # vdos=compute_vdos(w_true,w,sigma=eta/2,kernel='lorentz')
+    # ax.plot(w,vdos,'--',label='direct Lor.')
+    vdos=compute_vdos(w_true,w,sigma=eta,kernel='DHO')
+    ax.plot(w,vdos,'-.',label='direct') #DHO
+    chemical_f=spectra.vs.atoms.get_chemical_symbols()[0]
+    N=spectra.vs.atoms.get_global_number_of_atoms()
+    plt.title(f'a{chemical_f} $N={N}$ $\eta={eta}$' )
+    plt.legend()
+    plt.ylabel('VDOS')
+    plt.xlabel('$\omega\mathrm{~(rad/ps)}$')
+    plt.show()
